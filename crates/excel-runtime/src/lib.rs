9b7ea5edcd050e5118fd9db3c6978791bc340ccc
seorii <me@seorii.page>
2026-05-31 21:49:51 +0900

    feat: enforce chart invoke entry registry

--- crates/excel-runtime/src/lib.rs
+++ crates/excel-runtime/src/lib.rs
@@ -8948,6 +8948,9 @@ impl ExcelRuntime {
     ) -> OmResult<OmValue> {
         let object = self.runtime_object(handle)?;
         let focus_surface = match &object {
+            RuntimeObjectKind::ChartObjects { .. } => Some("ChartObjects"),
+            RuntimeObjectKind::ChartObject { .. } => Some("ChartObject"),
+            RuntimeObjectKind::ShapeRange { .. } => Some("ShapeRange"),
             RuntimeObjectKind::Chart { .. } => Some("Chart"),
             RuntimeObjectKind::ChartArea { .. } => Some("ChartArea"),
             RuntimeObjectKind::PlotArea { .. } => Some("PlotArea"),
@@ -8959,13 +8962,20 @@ impl ExcelRuntime {
             RuntimeObjectKind::ChartFormatChild { kind, .. } => Some(kind.surface_name()),
             RuntimeObjectKind::DataLabels { .. } => Some("DataLabels"),
             RuntimeObjectKind::DataLabel { .. } => Some("DataLabel"),
+            RuntimeObjectKind::ChartGroups { .. } => Some("ChartGroups"),
+            RuntimeObjectKind::ChartGroup { .. } => Some("ChartGroup"),
             RuntimeObjectKind::ChartGroupLines { kind, .. } => Some(kind.surface_name()),
+            RuntimeObjectKind::CategoryCollection { .. } => Some("CategoryCollection"),
+            RuntimeObjectKind::ChartCategory { .. } => Some("ChartCategory"),
+            RuntimeObjectKind::Axes { .. } => Some("Axes"),
             RuntimeObjectKind::Axis { .. } => Some("Axis"),
             RuntimeObjectKind::AxisTitle { .. } => Some("AxisTitle"),
             RuntimeObjectKind::DisplayUnitLabel { .. } => Some("DisplayUnitLabel"),
             RuntimeObjectKind::TickLabels { .. } => Some("TickLabels"),
             RuntimeObjectKind::Gridlines { .. } => Some("Gridlines"),
+            RuntimeObjectKind::SeriesCollection { .. } => Some("SeriesCollection"),
             RuntimeObjectKind::Series { .. } => Some("Series"),
+            RuntimeObjectKind::Points { .. } => Some("Points"),
             RuntimeObjectKind::Point { .. } => Some("Point"),
             _ => None,
         };
