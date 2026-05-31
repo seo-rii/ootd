50a1141b2b1d0a3f2fad8fc5a91dad30923a1d3c
seorii <me@seorii.page>
2026-05-31 22:04:02 +0900

    refactor: share chart focus surface mapping

--- crates/excel-runtime/src/lib.rs
+++ crates/excel-runtime/src/lib.rs
@@ -2287,38 +2287,7 @@ impl ExcelRuntime {
         args: &[OmValue],
     ) -> OmResult<OmValue> {
         let object = self.runtime_object(handle)?;
-        let focus_surface = match &object {
-            RuntimeObjectKind::ChartObjects { .. } => Some("ChartObjects"),
-            RuntimeObjectKind::ChartObject { .. } => Some("ChartObject"),
-            RuntimeObjectKind::ShapeRange { .. } => Some("ShapeRange"),
-            RuntimeObjectKind::Chart { .. } => Some("Chart"),
-            RuntimeObjectKind::ChartArea { .. } => Some("ChartArea"),
-            RuntimeObjectKind::PlotArea { .. } => Some("PlotArea"),
-            RuntimeObjectKind::ChartTitle { .. } => Some("ChartTitle"),
-            RuntimeObjectKind::Legend { .. } => Some("Legend"),
-            RuntimeObjectKind::DataTable { .. } => Some("DataTable"),
-            RuntimeObjectKind::ChartFormat { .. } => Some("ChartFormat"),
-            RuntimeObjectKind::Adjustments { .. } => Some("Adjustments"),
-            RuntimeObjectKind::ChartFormatChild { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::DataLabels { .. } => Some("DataLabels"),
-            RuntimeObjectKind::DataLabel { .. } => Some("DataLabel"),
-            RuntimeObjectKind::ChartGroups { .. } => Some("ChartGroups"),
-            RuntimeObjectKind::ChartGroup { .. } => Some("ChartGroup"),
-            RuntimeObjectKind::ChartGroupLines { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::CategoryCollection { .. } => Some("CategoryCollection"),
-            RuntimeObjectKind::ChartCategory { .. } => Some("ChartCategory"),
-            RuntimeObjectKind::Axes { .. } => Some("Axes"),
-            RuntimeObjectKind::Axis { .. } => Some("Axis"),
-            RuntimeObjectKind::TickLabels { .. } => Some("TickLabels"),
-            RuntimeObjectKind::AxisTitle { .. } => Some("AxisTitle"),
-            RuntimeObjectKind::DisplayUnitLabel { .. } => Some("DisplayUnitLabel"),
-            RuntimeObjectKind::Gridlines { .. } => Some("Gridlines"),
-            RuntimeObjectKind::SeriesCollection { .. } => Some("SeriesCollection"),
-            RuntimeObjectKind::Series { .. } => Some("Series"),
-            RuntimeObjectKind::Points { .. } => Some("Points"),
-            RuntimeObjectKind::Point { .. } => Some("Point"),
-            _ => None,
-        };
+        let focus_surface = Self::runtime_object_focus_surface(&object);
         if let Some(surface) = focus_surface
             && self.focus_member_declared(surface, member)
         {
@@ -2757,38 +2726,7 @@ impl ExcelRuntime {
         args: &[OmValue],
     ) -> OmResult<()> {
         let object = self.runtime_object(handle)?;
-        let focus_surface = match &object {
-            RuntimeObjectKind::ChartObjects { .. } => Some("ChartObjects"),
-            RuntimeObjectKind::ChartObject { .. } => Some("ChartObject"),
-            RuntimeObjectKind::ShapeRange { .. } => Some("ShapeRange"),
-            RuntimeObjectKind::Chart { .. } => Some("Chart"),
-            RuntimeObjectKind::ChartArea { .. } => Some("ChartArea"),
-            RuntimeObjectKind::PlotArea { .. } => Some("PlotArea"),
-            RuntimeObjectKind::ChartTitle { .. } => Some("ChartTitle"),
-            RuntimeObjectKind::Legend { .. } => Some("Legend"),
-            RuntimeObjectKind::DataTable { .. } => Some("DataTable"),
-            RuntimeObjectKind::ChartFormat { .. } => Some("ChartFormat"),
-            RuntimeObjectKind::Adjustments { .. } => Some("Adjustments"),
-            RuntimeObjectKind::ChartFormatChild { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::DataLabels { .. } => Some("DataLabels"),
-            RuntimeObjectKind::DataLabel { .. } => Some("DataLabel"),
-            RuntimeObjectKind::ChartGroups { .. } => Some("ChartGroups"),
-            RuntimeObjectKind::ChartGroup { .. } => Some("ChartGroup"),
-            RuntimeObjectKind::ChartGroupLines { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::CategoryCollection { .. } => Some("CategoryCollection"),
-            RuntimeObjectKind::ChartCategory { .. } => Some("ChartCategory"),
-            RuntimeObjectKind::Axes { .. } => Some("Axes"),
-            RuntimeObjectKind::Axis { .. } => Some("Axis"),
-            RuntimeObjectKind::TickLabels { .. } => Some("TickLabels"),
-            RuntimeObjectKind::AxisTitle { .. } => Some("AxisTitle"),
-            RuntimeObjectKind::DisplayUnitLabel { .. } => Some("DisplayUnitLabel"),
-            RuntimeObjectKind::Gridlines { .. } => Some("Gridlines"),
-            RuntimeObjectKind::SeriesCollection { .. } => Some("SeriesCollection"),
-            RuntimeObjectKind::Series { .. } => Some("Series"),
-            RuntimeObjectKind::Points { .. } => Some("Points"),
-            RuntimeObjectKind::Point { .. } => Some("Point"),
-            _ => None,
-        };
+        let focus_surface = Self::runtime_object_focus_surface(&object);
         if let Some(surface) = focus_surface
             && self.focus_member_declared(surface, member)
             && !matches!(
@@ -8968,38 +8906,7 @@ impl ExcelRuntime {
         args: &[OmValue],
     ) -> OmResult<OmValue> {
         let object = self.runtime_object(handle)?;
-        let focus_surface = match &object {
-            RuntimeObjectKind::ChartObjects { .. } => Some("ChartObjects"),
-            RuntimeObjectKind::ChartObject { .. } => Some("ChartObject"),
-            RuntimeObjectKind::ShapeRange { .. } => Some("ShapeRange"),
-            RuntimeObjectKind::Chart { .. } => Some("Chart"),
-            RuntimeObjectKind::ChartArea { .. } => Some("ChartArea"),
-            RuntimeObjectKind::PlotArea { .. } => Some("PlotArea"),
-            RuntimeObjectKind::ChartTitle { .. } => Some("ChartTitle"),
-            RuntimeObjectKind::Legend { .. } => Some("Legend"),
-            RuntimeObjectKind::DataTable { .. } => Some("DataTable"),
-            RuntimeObjectKind::ChartFormat { .. } => Some("ChartFormat"),
-            RuntimeObjectKind::Adjustments { .. } => Some("Adjustments"),
-            RuntimeObjectKind::ChartFormatChild { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::DataLabels { .. } => Some("DataLabels"),
-            RuntimeObjectKind::DataLabel { .. } => Some("DataLabel"),
-            RuntimeObjectKind::ChartGroups { .. } => Some("ChartGroups"),
-            RuntimeObjectKind::ChartGroup { .. } => Some("ChartGroup"),
-            RuntimeObjectKind::ChartGroupLines { kind, .. } => Some(kind.surface_name()),
-            RuntimeObjectKind::CategoryCollection { .. } => Some("CategoryCollection"),
-            RuntimeObjectKind::ChartCategory { .. } => Some("ChartCategory"),
-            RuntimeObjectKind::Axes { .. } => Some("Axes"),
-            RuntimeObjectKind::Axis { .. } => Some("Axis"),
-            RuntimeObjectKind::AxisTitle { .. } => Some("AxisTitle"),
-            RuntimeObjectKind::DisplayUnitLabel { .. } => Some("DisplayUnitLabel"),
-            RuntimeObjectKind::TickLabels { .. } => Some("TickLabels"),
-            RuntimeObjectKind::Gridlines { .. } => Some("Gridlines"),
-            RuntimeObjectKind::SeriesCollection { .. } => Some("SeriesCollection"),
-            RuntimeObjectKind::Series { .. } => Some("Series"),
-            RuntimeObjectKind::Points { .. } => Some("Points"),
-            RuntimeObjectKind::Point { .. } => Some("Point"),
-            _ => None,
-        };
+        let focus_surface = Self::runtime_object_focus_surface(&object);
         if let Some(surface) = focus_surface
             && self.focus_member_declared(surface, member)
         {
@@ -15134,6 +15041,41 @@ impl ExcelRuntime {
     fn set_headless_cut_mode(&mut self) {
         self.cut_copy_mode = Some(XL_CUT);
         self.clipboard = None;
+    }
+
+    fn runtime_object_focus_surface(object: &RuntimeObjectKind) -> Option<&'static str> {
+        match object {
+            RuntimeObjectKind::ChartObjects { .. } => Some("ChartObjects"),
+            RuntimeObjectKind::ChartObject { .. } => Some("ChartObject"),
+            RuntimeObjectKind::ShapeRange { .. } => Some("ShapeRange"),
+            RuntimeObjectKind::Chart { .. } => Some("Chart"),
+            RuntimeObjectKind::ChartArea { .. } => Some("ChartArea"),
+            RuntimeObjectKind::PlotArea { .. } => Some("PlotArea"),
+            RuntimeObjectKind::ChartTitle { .. } => Some("ChartTitle"),
+            RuntimeObjectKind::Legend { .. } => Some("Legend"),
+            RuntimeObjectKind::DataTable { .. } => Some("DataTable"),
+            RuntimeObjectKind::ChartFormat { .. } => Some("ChartFormat"),
+            RuntimeObjectKind::Adjustments { .. } => Some("Adjustments"),
+            RuntimeObjectKind::ChartFormatChild { kind, .. } => Some(kind.surface_name()),
+            RuntimeObjectKind::DataLabels { .. } => Some("DataLabels"),
+            RuntimeObjectKind::DataLabel { .. } => Some("DataLabel"),
+            RuntimeObjectKind::ChartGroups { .. } => Some("ChartGroups"),
+            RuntimeObjectKind::ChartGroup { .. } => Some("ChartGroup"),
+            RuntimeObjectKind::ChartGroupLines { kind, .. } => Some(kind.surface_name()),
+            RuntimeObjectKind::CategoryCollection { .. } => Some("CategoryCollection"),
+            RuntimeObjectKind::ChartCategory { .. } => Some("ChartCategory"),
+            RuntimeObjectKind::Axes { .. } => Some("Axes"),
+            RuntimeObjectKind::Axis { .. } => Some("Axis"),
+            RuntimeObjectKind::TickLabels { .. } => Some("TickLabels"),
+            RuntimeObjectKind::AxisTitle { .. } => Some("AxisTitle"),
+            RuntimeObjectKind::DisplayUnitLabel { .. } => Some("DisplayUnitLabel"),
+            RuntimeObjectKind::Gridlines { .. } => Some("Gridlines"),
+            RuntimeObjectKind::SeriesCollection { .. } => Some("SeriesCollection"),
+            RuntimeObjectKind::Series { .. } => Some("Series"),
+            RuntimeObjectKind::Points { .. } => Some("Points"),
+            RuntimeObjectKind::Point { .. } => Some("Point"),
+            _ => None,
+        }
     }
 
     fn focus_member_declared(&self, surface: &str, member: &str) -> bool {
