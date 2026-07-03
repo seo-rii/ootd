d6b7e31e7feb09f285dd912387b390146e21ca74
seorii <me@seorii.page>
2026-07-03 21:49:12 +0900

    test: cover object defaults deep name drift

--- crates/excel-xlsx/src/lib.rs
+++ crates/excel-xlsx/src/lib.rs
@@ -107346,6 +107346,82 @@ mod tests {
     }
 
     #[test]
+    fn save_rejects_theme_part_when_object_defaults_great_great_grandchild_name_drifts() {
+        let codec = XlsxCodec;
+        let input = workbook_with_theme_object_defaults_great_great_grandchild_bytes();
+        let mut loaded = codec
+            .load(&input, CommonLoadOptions::default())
+            .expect("load workbook");
+        let sheet_id = loaded.state.worksheets[0].id;
+        let theme_xml = String::from_utf8(
+            loaded
+                .package
+                .part("xl/theme/theme1.xml")
+                .expect("theme part")
+                .bytes
+                .clone(),
+        )
+        .expect("theme xml utf8")
+        .replace("<a:alpha val=\"50000\"/>", "<a:alphaMod val=\"50000\"/>");
+        loaded
+            .package
+            .replace_part_bytes("xl/theme/theme1.xml", theme_xml.into_bytes())
+            .expect("replace theme part");
+        loaded
+            .state
+            .set_range_values(
+                &office_common::RangeRef::single_cell(WorkbookId(0), sheet_id, 1, 1),
+                &office_common::OmArray::scalar(office_common::OmValue::Number(9.0)),
+            )
+            .expect("set value");
+
+        let error = codec
+            .save(&loaded, office_common::SaveOptions::default())
+            .expect_err("save should fail when objectDefaults great-great-grandchild name drifts");
+        assert_eq!(error.code, OmErrorCode::InvalidState);
+        assert!(error.message.contains("typed theme summary drifted"));
+        assert!(error.message.contains("xl/theme/theme1.xml"));
+    }
+
+    #[test]
+    fn save_rejects_theme_part_when_object_defaults_great_great_grandchild_set_drifts() {
+        let codec = XlsxCodec;
+        let input = workbook_with_theme_object_defaults_great_great_grandchild_bytes();
+        let mut loaded = codec
+            .load(&input, CommonLoadOptions::default())
+            .expect("load workbook");
+        let sheet_id = loaded.state.worksheets[0].id;
+        let theme_xml = String::from_utf8(
+            loaded
+                .package
+                .part("xl/theme/theme1.xml")
+                .expect("theme part")
+                .bytes
+                .clone(),
+        )
+        .expect("theme xml utf8")
+        .replace("<a:alpha val=\"50000\"/>", "");
+        loaded
+            .package
+            .replace_part_bytes("xl/theme/theme1.xml", theme_xml.into_bytes())
+            .expect("replace theme part");
+        loaded
+            .state
+            .set_range_values(
+                &office_common::RangeRef::single_cell(WorkbookId(0), sheet_id, 1, 1),
+                &office_common::OmArray::scalar(office_common::OmValue::Number(9.0)),
+            )
+            .expect("set value");
+
+        let error = codec
+            .save(&loaded, office_common::SaveOptions::default())
+            .expect_err("save should fail when objectDefaults great-great-grandchild set drifts");
+        assert_eq!(error.code, OmErrorCode::InvalidState);
+        assert!(error.message.contains("typed theme summary drifted"));
+        assert!(error.message.contains("xl/theme/theme1.xml"));
+    }
+
+    #[test]
     fn save_rejects_theme_part_when_object_defaults_great_great_grandchild_text_drifts() {
         let codec = XlsxCodec;
         let input = workbook_with_theme_object_defaults_great_great_grandchild_text_bytes();
